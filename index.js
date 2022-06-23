5035524ec1ea4137e69db9e4e586f758dcd17ca1
Andrew Byrd <andrew.byrds2@gmail.com>
2022-06-23 18:58:50 -0400

    Got most of it to work, just need to work on license badges

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// TODO: Include packages needed for this application
-
 // External packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
 // Internal packages
-const api = require('.utils/api.js');
-const generateMarkdown = require('./utils/generateMarkdown.js')
-
-// TODO: Create an array of questions for user input
+const api = require('./utils/git-api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Questions for user input
 const questions = [
@@ -68,38 +64,71 @@ const questions = [
         type: 'input',
         message: "If applicable, describe the steps a user needs to follow to install your project.",
         name: 'install',
-    }
+    },
 
     {
         type: 'input',
         message: "Please provide instructions and examples of how your project will be used.",
         name: 'usage',
-    }
+    },
 
     {
         type: 'input',
-        message: "If applicable, describe the steps a user needs to follow to install your project.",
+        message: "Please select a license for your project.",
+        choices: ["MIT", "GPLv3", "GPL"],
         name: 'license',
-    }
+    },
 
     {
         type: 'input',
-        message: "If applicable, describe the steps a user needs to follow to install your project.",
+        message: "If applicable, describe how others can contribute to your project.",
         name: 'contributing',
-    }
+    },
 
     {
         type: 'input',
-        message: "If applicable, describe the steps a user needs to follow to install your project.",
+        message: "If applicable, provide any tests you have for your project and how they can be ran.",
         name: 'tests',
-    }
+    },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Genrates README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('Awesome, your README file has been generated :)')
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+
+    try {
+        const userResponses = await inquirer.prompt(questions);
+        console.log('Your responses: ', userResponses);
+        console.log('Okay, fetching your GitHub data now...');
+
+        // Call GitHub api
+        const userInfo = await api.getUser(userResponses);
+        console.log('Your GitHub account info: ', userInfo);
+
+        //  Pass the responses and GitHub info to generateMarkdown
+        console.log('Generating that README...');
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+
+        //  Apply markdown to README file
+        await writeFileAsync('yourNewREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 // Function call to initialize app
 init();
---
+++ yourNewREADME.md
@@ -0,0 +1,28 @@
+# Portfolio
+
+  ## Description
+  *A little about this project.*
+
+  hehe
+  ## Table of Contents
+  * [Installation](#installation)
+  * [License](#license)
+
+    ## Installation
+
+    *Steps to istalling and running this project:*
+
+    undefined
+
+  ## License
+
+  
+  
+  ---
+
+  ## Questions?
+
+  For any questions, please contact me with the information below:
+
+  GitHub: [@Andrew-Byrd49](https://api.github.com/users/Andrew-Byrd49)
+  
